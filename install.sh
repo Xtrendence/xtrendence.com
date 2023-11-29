647f2bc1a96a1cecb6efe84751112262c809793e
Xtrendence <xtrendence@gmail.com>
2023-11-29 02:21:44 +0000

    Fixed install script.

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ cd ~/Documents/tools/dns-updater && npm install;
 cd ~/Documents/tools/stake-check && npm install;
 
 cd ~/Documents/website/ && npm install;
-cd ~/Documents/website/tools/lights && npm install;
+cd ~/Documents/website/modules/tools/lights && npm install;
