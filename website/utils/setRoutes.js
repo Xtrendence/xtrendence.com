2a20efe9fdd54aa5b60e539b06a6f5457bc8f819
Xtrendence <xtrendence@gmail.com>
2023-11-30 17:29:07 +0000

    Added Borat MP3 file.

--- website/utils/setRoutes.js
+++ website/utils/setRoutes.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function setRoutes(app) {
     app.use(express.static('public'));
@@ -17,6 +22,10 @@ export function setRoutes(app) {
 
     app.get(['/portfolio', '/portfolio/*'], (_, res) => {
         res.redirect('https://xtrendence.dev');
+    });
+
+    app.get('/www/extras/borat.mp3', (_, res) => {
+        res.sendFile(path.join(__dirname, '../public/assets/audio/borat.mp3'));
     });
 
     app.get('/error/:code', (req, res) => {
