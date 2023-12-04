55584587633765a3d155fca2dd39c82df68b8e3b
Xtrendence <xtrendence@gmail.com>
2023-12-04 21:36:57 +0000

    Added update script.

---
+++ update.sh
@@ -0,0 +1,10 @@
+git pull origin master;
+
+cd ~/Documents/tools/dns-updater && npm install;
+cd ~/Documents/tools/stake-check && npm install;
+
+cd ~/Documents/website/ && npm install;
+cd ~/Documents/website/modules/tools/lights && npm install;
+cd ~/Documents/website/modules/auth && npm install;
+
+sh ~/Documents/website/start.sh;
