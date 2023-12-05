abe270abd2ef5f2a696b49165b22fac3455e8826
Xtrendence <xtrendence@gmail.com>
2023-12-05 19:31:28 +0000

    Stake check no longer actively runs.

--- website/start.sh
+++ website/start.sh
@@ -1,15 +1,12 @@
 pm2 stop xtrendence.com; 
 pm2 stop auth; 
-pm2 stop stake-check; 
 pm2 stop lights; 
 
 pm2 delete xtrendence.com; 
 pm2 delete auth; 
-pm2 delete stake-check;
 pm2 delete lights;
 
 cd ~/Documents/website && pm2 start npm --name "xtrendence.com" -- run prod;
-cd ~/Documents/tools/stake-check && pm2 start npm --name "stake-check" -- run start;
 cd ~/Documents/website/modules/tools/lights && pm2 start npm --name "lights" -- run start;
 cd ~/Documents/website/modules/auth && pm2 start npm --name "auth" -- run start;
 
--- website/stop.sh
+++ website/stop.sh
@@ -1,11 +1,9 @@
 pm2 stop xtrendence.com; 
 pm2 stop auth; 
-pm2 stop stake-check; 
 pm2 stop lights;
 
 pm2 delete xtrendence.com;
 pm2 delete auth;
-pm2 delete stake-check;
 pm2 delete lights;
 
 pm2 save;
